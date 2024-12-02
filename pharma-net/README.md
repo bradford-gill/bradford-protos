017a2ae9926718cede41e921c1ba0b8894bd0beb
Bradford Gill <bradfordgill@Bradfords-MacBook-Pro.local>
2024-12-01 20:12:01 -0500

    Update port

--- pharma-net/README.md
+++ pharma-net/README.md
@@ -5,7 +5,8 @@ Prototype to provide insights into high end pharmaceutical
 ### Docker 
 ~~~bash
 docker build -t proto .
-docker run --env-file .env -p 8080:8080 proto
+# if .env file !exist -> nano .env
+docker run -d --env-file .env -p 8080:8080 proto
 
 ~~~
 ### Conda on local 
@@ -23,3 +24,6 @@ source .env
 # run streamlit
 streamlit run app.py
 ~~~
+
+
+
--- pharma-net/app.py
+++ pharma-net/app.py
@@ -6,6 +6,11 @@ import itertools
 
 from utils import check_password
 
+st.set_page_config(
+    page_title="Bradford Gill | Pharma-net",
+    page_icon='⚕️',
+)
+
 prompts = [
     f"Tell me about {drug}" for drug in [
         "Pembrolizumab (Keytruda)",
@@ -21,6 +26,7 @@ prompts = [
 
 class Session:
     def __init__(self) -> None:
+        
         self.reset()
         
         self.client = OpenAI(
@@ -28,7 +34,6 @@ class Session:
         )
     
     def reset(self) -> None:
-        print('NEW SESSION')
         self.messages = [
             {
                 'role': 'system',
@@ -70,38 +75,44 @@ class Session:
 
         return chat_completion.choices[0].message.content
         
-session = Session()
+if 'session' not in st.session_state:
+    st.session_state['session'] = Session()
 
 # Streamlit app
 def main():
-    st.title("Pharma-net prototype")
+    st.title("Pharma-net Prototype")
+    st.warning("FOR PROTOTYPE USE ONLY - RESULT ACCURACY NOT GUARANTEED")
     
     if not check_password():  
         st.stop()
+        
  
     st.markdown("""
     Welcome to the Pharma-net prototype created by Bradford Gill.
     
-    This is to be used as a proof of concept for prototyping only. Please expect errors in answers and a less than optimized user experience.
+    This is to be used as a proof of concept for prototyping only. 
+    Please expect some errors and a less than optimized user experience.
     
     Thank you for your consideration, feedback is appreciated. 
+    
+    **Use Directions:**
+    
+    Pretend you are having a conversation with the model. For example; 
+    if you are asking about the specific drug, ask clarifying questions
+    about the drug such as 'can you please expand on the side effects'.
     
     **Contact**: (413)-575-3769
     """)
     
-    st.warning("FOR PROTOTYPE USE ONLY - RESULT ACCURACY MAY VARY")
-    
-
   
     if st.button("Reset Session"):
-        session.reset()
+        st.session_state['session'].reset()
         st.rerun()
 
     
     # Input field
     st.markdown("""
     #### I am an LLM trained on high-end pharmaceuticals, how can I help you?
-    Please enter your own prompts to experiment with the model
     """)
     
     custom_input: bool = st.toggle("Custom Input", value=True)
@@ -117,24 +128,10 @@ def main():
     if submit:
         # Call the get_response function
         with st.spinner("Running AI model"):
-            message = session.get_response(user_input)
+            message = st.session_state['session'].get_response(user_input)
 
         # Display the response in markdown
         st.markdown(message)
-
-    
-    
-    # >> Chat history >>
-        
-    if len(session.messages) > 1:
-        st.markdown(" \n " * 8)
-        st.markdown("### Chat history: ")
-        
-    for message in session.messages[1:]: 
-        if message['role'] == 'system':
-            st.markdown(message["content"])
-        else: 
-            st.markdown(f"_{message['content']}_")
 
 
 if __name__ == "__main__":
